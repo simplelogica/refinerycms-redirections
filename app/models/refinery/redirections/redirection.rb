ec7b283057b30232eab2a53f7e1043c25c9fea0a
David J. Brenes <david.brenes@simplelogica.net>
2013-08-26 15:22:51 +0200

    Validations for required attributes

--- app/models/refinery/redirections/redirection.rb
+++ app/models/refinery/redirections/redirection.rb
@@ -1,4 +1,4 @@
-puts "HEY YA"
 class Refinery::Redirections::Redirection < ActiveRecord::Base
   attr_accessible :from_url, :status_code, :to_url
+  validates :from_url, :status_code, :to_url, presence: true
 end
--- spec/models/redirection_spec.rb
+++ spec/models/redirection_spec.rb
@@ -4,6 +4,7 @@ module Refinery
   module Redirections
     describe Redirection do
       it_behaves_like "model with attributes", :redirection
+      it_behaves_like "model with required attributes", :redirection, [:from_url, :status_code, :to_url]
     end
   end
 end
