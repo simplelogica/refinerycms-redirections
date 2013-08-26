3b9164f183ef93457c16fc9ba8c69ae5ed82efa5
David J. Brenes <david.brenes@simplelogica.net>
2013-08-26 16:40:15 +0200

    I almost forget to sanitize paths when saving a redirection

--- app/models/refinery/redirections/redirection.rb
+++ app/models/refinery/redirections/redirection.rb
@@ -3,6 +3,14 @@ class Refinery::Redirections::Redirection < ActiveRecord::Base
 
   validates :from_url, :status_code, :to_url, presence: true
 
+
+  before_save :sanitize_path
+
+  def sanitize_path
+    self.from_url = self.class.sanitize_path from_url
+  end
+
+
   # This method gets a path and 'sanitizes it'. This means that all the params that we don't want (Google Analytics params, testing params...) are stripped out and params are sorted so '/en?a=1&b=1' and '/en?b=1&a=1' are the same
   def self.sanitize_path path, ignored_path_params = Refinery::Redirections.ignored_path_params
     path, params = path.split("?")
