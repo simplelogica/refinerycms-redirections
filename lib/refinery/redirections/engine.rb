4f7a694f3e1b93fe3005ea821355c2b83834573c
David J. Brenes <david.brenes@simplelogica.net>
2013-08-27 15:32:23 +0200

    Adding configuration to disable rack rewrite or the controller

--- lib/refinery/redirections/engine.rb
+++ lib/refinery/redirections/engine.rb
@@ -19,13 +19,16 @@ module Refinery
         Refinery.register_engine(Refinery::Redirections)
       end
 
+      if Refinery::Redirections.enable_rack_redirection
 
-      initializer 'add rack rewrite rules' do |app|
-        app.config.middleware.insert_before(Rack::Lock, Rack::Rewrite) do
-          Refinery::Redirections::Redirection.all.each do |redirection|
-            send("r#{redirection.status_code}", redirection.from_url, redirection.to_url)
+        initializer 'add rack rewrite rules' do |app|
+          app.config.middleware.insert_before(Rack::Lock, Rack::Rewrite) do
+            Refinery::Redirections::Redirection.all.each do |redirection|
+              send("r#{redirection.status_code}", redirection.from_url, redirection.to_url)
+            end
           end
         end
+
       end
 
       paths["config/locales"] << "config/locales/**"
