b0bd90553b1bdb7172651f8df0ae89fa2728600a
David J. Brenes <david.brenes@simplelogica.net>
2013-08-27 15:50:36 +0200

    Comparing string and integer in an spec

--- spec/requests/redirections_spec.rb
+++ spec/requests/redirections_spec.rb
@@ -16,7 +16,7 @@ module Refinery
       context "when visiting a url with a redirection created" do
 
         before { visit_without_redirects redirection.from_url }
-        it('should redirect with the right http status code') { page.status_code.should eql redirection.status_code }
+        it('should redirect with the right http status code') { page.status_code.to_s.should eql redirection.status_code }
         it('should redirect to the right path') { redirected_url.should eql "http://www.example.com#{redirection.to_url}" }
 
       end
